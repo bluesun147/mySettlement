f1a31bf591b88b59a9fd02611d457b02619b245e
bluesun147 <bluesun147@naver.com>
2024-03-20 21:49:09 +0900

    [Refactor] Content 서비스는 분리 후 배포
    https://github.com/bluesun147/mySettlement

--- content/src/main/java/com/haechan/content/domain/ost/repository/OstRepository.java
+++
@@ -1,7 +0,0 @@
-package com.haechan.content.domain.ost.repository;
-
-import com.haechan.content.domain.ost.entity.Ost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OstRepository extends JpaRepository<Ost, Long> {
-}
